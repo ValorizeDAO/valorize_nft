84c60c8fee104a482d80f0a4008911b37fa2ef20
Marco Huberts <marcohuberts@iMac-van-Marco-2.local>
2022-06-29 17:24:01 +0200

    first commit including hardhat config, package.json & package-lock.json

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import "@nomiclabs/hardhat-waffle";
+import { task } from "hardhat/config";
+import "hardhat-typechain";
+import { ethers } from "hardhat";
+require('hardhat-exposed');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    const accountBalance = await account.getBalance()
+    console.log(account.address, "balance:", hre.ethers.utils.formatEther(accountBalance));
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default {
+  solidity: "0.8.6",
+};
