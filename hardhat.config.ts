e449ba374b76024a7a273dc5cb5ca20505940782
Marco Huberts <marcohuberts@iMac-van-Marco-2.local>
2022-06-29 20:55:10 +0200

    should not have pushed to staging

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import "@nomiclabs/hardhat-waffle";
 import { task } from "hardhat/config";
 import "hardhat-typechain";
 import { ethers } from "hardhat";
-require('hardhat-exposed');
+//require('hardhat-exposed');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
