9f8eacd6d41f0e0f97b38c87f42700ea17ec1b76
Marrmee <mhuberts2008@hotmail.com>
2022-09-17 00:46:20 +0200

    improved generateMetadata.js by Javi

--- arguments.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  "https://token-cdn-domain/",
-  "0x8a7ad9A192CbB31679D0d468c25546F2949c8BB1", //royaltyDistributorAddress
-  "0xCD892dA81cB7a981bf6B841d07f2467585E423DB", //artistAddress
-  12,
-  1012,
-  2012,
-];
